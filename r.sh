9a9445ff17ffbd246f8aebb68a84f85c6522d029
lberry <bu_le@foxmail.com>
2019-01-24 23:55:45 +0800

    Update r.sh

--- r.sh
+++ r.sh
@@ -8,3 +8,6 @@ pip install git+https://github.com/shadowsocks/shadowsocks.git@master
 wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && ./bbr.sh
 
 iptables -A INPUT -p tcp --tcp-flags RST RST -j DROP
+
+#Create /etc/sysctl.d/local.conf 
+#then sysctl --system
