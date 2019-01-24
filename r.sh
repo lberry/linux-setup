41e7f303a2b8a44d4ab1c803cc0a61c26782acd3
lberry <bu_le@foxmail.com>
2019-01-24 23:49:35 +0800

    Update r.sh

--- r.sh
+++ r.sh
@@ -6,3 +6,5 @@ apt-get install python-pip
 pip install git+https://github.com/shadowsocks/shadowsocks.git@master
 
 wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && ./bbr.sh
+
+iptables -A INPUT -p tcp --tcp-flags RST RST -j DROP
