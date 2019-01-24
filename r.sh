18650b38035d04fd3adaa10f2a98444d6d1442c4
lberry <bu_le@foxmail.com>
2019-01-24 18:11:46 +0800

    Create r.sh

---
+++ r.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+service ssh stop
+
+apt-get install python-pip
+pip install git+https://github.com/shadowsocks/shadowsocks.git@master
+
+wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && ./bbr.sh
