12cd2ee27d58c0373fa8fb74cd856464bf724ab4
lberry <bu_le@foxmail.com>
2019-03-12 14:09:55 +0800

    Update r.sh

--- r.sh
+++ r.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-service ssh stop
+#service ssh stop
 
 #enable bbr
 wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && ./bbr.sh
@@ -19,8 +19,14 @@ pip install git+https://github.com/shadowsocks/shadowsocks.git@master
 #block ssh &etc
 #setup /etc/ssh/sshd_config
 #setup /etc/hosts.deny&allow
+#/etc/ssh/sshd_config 
+#AllowUsers root@www.xxx.com
 
 #shut ipv6
 sudo sh -c 'echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6'
 
 #ban crack ip
+wget https://raw.githubusercontent.com/shadowsocks/shadowsocks/master/utils/autoban.py
+python autoban.py < /var/log/shadowsocks.log
+nohup tail -F /var/log/shadowsocks.log | python autoban.py >log 2>log &
+
